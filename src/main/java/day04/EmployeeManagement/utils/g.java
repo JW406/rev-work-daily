b07fdb1992e58430383c0c815a089e10dbb2656f
jw904 <jwvic.2357@gmail.com>
2021-06-16 21:11:49 -0400

    employee proj: user input wrapper that takes care about exceptions and invalid input

--- src/main/java/day04/EmployeeManagement/utils/g.java
+++ src/main/java/day04/EmployeeManagement/utils/g.java
@@ -24,6 +24,38 @@ public class g {
     return res;
   }
 
+  public static String getNextString() {
+    String res = "";
+    while (true) {
+      try {
+        res = sc.nextLine().trim();
+        if (!res.equals("")) {
+          break;
+        }
+      } catch (NoSuchElementException e) {
+        Exit();
+      }
+    }
+    return res;
+  }
+
+  public static double getNextDouble() {
+    double res;
+    while (true) {
+      try {
+        res = sc.nextDouble();
+        sc.nextLine();
+        break;
+      } catch (InputMismatchException e) {
+        sc.nextLine();
+        System.out.println("Invalid input, please enter a valid number");
+      } catch (NoSuchElementException e) {
+        Exit();
+      }
+    }
+    return res;
+  }
+
   public static void Exit() {
     System.out.println("\nThank you for using the software.\n");
     System.exit(0);
