f3b9ab5bc0ac4620417fdbf82d9335e7eb969e8c
jw904 <jwvic.2357@gmail.com>
2021-06-18 12:27:53 -0400

    employee proj: should move some objs in main to class static

--- src/main/java/EmployeeManagement/Main.java
+++ src/main/java/EmployeeManagement/Main.java
@@ -14,20 +14,19 @@ import EmployeeManagement.utils.g;
 
 public class Main {
   private static final Logger logger = Logger.getLogger(Main.class.getName());
+  private static final Map<Integer, Object[]> UIMap = new TreeMap<Integer, Object[]>() {
+    {
+      put(0, new Object[] { "0. Add an employee", Displayer.get("AddEmp") });
+      put(1, new Object[] { "1. List all employee", Displayer.get("ListEmps") });
+      put(2, new Object[] { "2. Display Yearly Salary", Displayer.get("DisplayYearlySal") });
+      put(3, new Object[] { "3. Display Specific Employee Detail", Displayer.get("DisplayAnEmployee") });
+      put(4, new Object[] { "4. Modify Employee Detail", Displayer.get("ModifyAnEmployee") });
+      put(5, new Object[] { "5. Delete an Employee", Displayer.get("DeleteAnEmployee") });
+    }
+  };
+  private static final EmployeeService svc = new EmployeeServiceImpl();
 
   public static void main(String[] args) {
-    EmployeeService svc = new EmployeeServiceImpl();
-    Map<Integer, Object[]> UIMap = new TreeMap<Integer, Object[]>() {
-      {
-        put(0, new Object[] { "0. Add an employee", Displayer.get("AddEmp") });
-        put(1, new Object[] { "1. List all employee", Displayer.get("ListEmps") });
-        put(2, new Object[] { "2. Display Yearly Salary", Displayer.get("DisplayYearlySal") });
-        put(3, new Object[] { "3. Display Specific Employee Detail", Displayer.get("DisplayAnEmployee") });
-        put(4, new Object[] { "4. Modify Employee Detail", Displayer.get("ModifyAnEmployee") });
-        put(5, new Object[] { "5. Delete an Employee", Displayer.get("DeleteAnEmployee") });
-      }
-    };
-
     while (true) {
       System.out.println();
       for (Entry<Integer, Object[]> e : UIMap.entrySet()) {
